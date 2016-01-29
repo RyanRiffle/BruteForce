69bd8b12af94ef5723e8b2740b84b37f5a9c0e38
ryanriffle <ryanriffle@icloud.com>
2016-01-29 19:28:17 +0900

    Fixed a few errors

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-  if (argc != 2) {
+  if (argc != 3) {
     cerr << "Usage: " << argv[0] << " [CHARSET] [MAX-LENGTH]" << endl;
     return 0;
   }
 
-  int length = 0;
-  cerr << "What is the max length: ";
-  cin >> length;
+  int length = atoi(argv[2]);
+  cerr << "Generating " << pow(strlen(argv[1]), length) << endl;
+  return 0;
 
-  int pos[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+  int *pos = new int[length];
+  for (int i = 0; i < length; i++) {
+	  pos[i] = -1;
+  }
   bool done = false;
 
   int y = 0;
   while (true) {
     for (int x = 0; x < strlen(argv[1]); x++) {
       pos[0] += 1;
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < length; i++) {
         if (pos[i] == -1) {
           break;
         }
@@ -32,7 +36,10 @@ int main(int argc, char *argv[])
           pos[i+1] += 1;
           pos[i] = 0;
         }
-        cout << argv[1][pos[i]];
+
+		if (!done) {
+			cout << argv[1][pos[i]];
+		}
       }
 
       cout << endl;
@@ -45,7 +52,7 @@ int main(int argc, char *argv[])
       break;
     }
   }
-
+  delete pos;
 
   return 0;
 }
